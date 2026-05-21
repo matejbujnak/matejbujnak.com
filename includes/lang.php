6e195a4fa3898f13f63617366c3873ca1f09108f
Matej Bujňák <bujnama3@fit.cvut.cz>
2026-05-21 15:36:18 +0200

    chore: translate all comments to English

--- includes/lang.php
+++ includes/lang.php
@@ -1,24 +1,24 @@
 <?php
-// Podporované jazyky
+// Supported languages
 const SUPPORTED_LANGS = ['cs', 'sk', 'en'];
 const DEFAULT_LANG    = 'cs';
 
-// Prepnutie jazyka cez ?lang=xx
+// Switch language via ?lang=xx
 if (isset($_GET['lang']) && in_array($_GET['lang'], SUPPORTED_LANGS, true)) {
     setcookie('lang', $_GET['lang'], time() + 60 * 60 * 24 * 365, '/');
     $_COOKIE['lang'] = $_GET['lang'];
 
-    // Redirect bez ?lang= v URL
+    // Redirect without ?lang= in URL
     $url = strtok($_SERVER['REQUEST_URI'], '?');
     header('Location: ' . $url);
     exit;
 }
 
-// Aktívny jazyk
+// Active language
 $lang = $_COOKIE['lang'] ?? DEFAULT_LANG;
 if (!in_array($lang, SUPPORTED_LANGS, true)) {
     $lang = DEFAULT_LANG;
 }
 
-// Načítanie prekladov
+// Load translations
 require_once __DIR__ . '/../lang/' . $lang . '.php';
